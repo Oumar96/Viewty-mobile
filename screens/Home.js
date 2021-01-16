f2c7b8996056322289176b22b84170f8f3ce9c60
steffan <steffan_v@hotmail.com>
2021-01-16 13:41:06 -0500

    #phone-auth requesting verification code and return result

--- screens/Home.js
+++ screens/Home.js
@@ -1,14 +1,71 @@
-import React from 'react';
-import {StyleSheet, View, Text, TouchableOpacity} from 'react-native';
 
-const Home = ({ navigation }) =>{
-    return(
+import React, {useState, useRef} from 'react';
+import {StyleSheet, View, Text, TextInput, TouchableOpacity} from 'react-native';
+import { FirebaseRecaptchaVerifierModal } from 'expo-firebase-recaptcha';
+import firebase from "../firebase/firebase.js";
+
+export default App = () => {
+
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [code, setCode] = useState('');
+  const [verificationId, setVerificationId] = useState('');
+  const recaptchaVerifier = useRef(null);
+
+// Function to be called when requesting for a verification code
+const sendVerification = () => {
+    const phoneProvider = new firebase.auth.PhoneAuthProvider();
+    phoneProvider
+        .verifyPhoneNumber(phoneNumber, recaptchaVerifier.current)
+        .then(setVerificationId);
+    };
+  // Function to be called when confirming the verification code that we received
+  // from Firebase via SMS
+  const confirmCode = () => {
+    const credential = firebase.auth.PhoneAuthProvider.credential(
+      verificationId,
+      code
+    );
+    firebase
+      .auth()
+      .signInWithCredential(credential)
+      .then((result) => {
+        // Do something with the results here
+        console.log(result);
+      });
+  }
+
+  return (
+
+    <>
         <View style={styles.container}>
-            <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Movies')}>
-                <Text style={styles.option}> Go to movies </Text>
+            <TextInput
+                placeholder="Phone Number"
+                onChangeText={setPhoneNumber}
+                autoCompleteType="tel"
+                style={styles.option}
+            />
+            <TouchableOpacity style={styles.button} onPress={sendVerification}>
+                <Text style={styles.option}>Send Verification Code</Text>
             </TouchableOpacity>
         </View>
-    )
+
+        <View style={styles.container}>
+            <TextInput
+                placeholder="Confirmation Code"
+                onChangeText={setCode}
+                style={styles.option}
+            />
+            <TouchableOpacity style={styles.button} onPress={confirmCode}>
+                <Text style={styles.option}>Verify Code</Text>
+            </TouchableOpacity>
+        </View>
+        <FirebaseRecaptchaVerifierModal
+        ref={recaptchaVerifier}
+        firebaseConfig={firebase.app().options}
+        />
+    </>
+
+  );
 }
 
 const styles = StyleSheet.create({
@@ -33,4 +90,3 @@ const styles = StyleSheet.create({
     }
   });
 
-export default Home;
