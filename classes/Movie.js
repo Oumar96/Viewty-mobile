1dd9c4d10150f95670fb09c08b22486ba3fc55c2
Oumar <oumarba221296@hotmail.fr>
2021-03-01 23:51:43 -0500

    Movies

---
+++ classes/Movie.js
@@ -0,0 +1,31 @@
+/**
+ * 
+ * @param {String} name 
+ * @param {String} description 
+ * @param {Number} likes 
+ * @param {String} poster 
+ * @param {Number} votes 
+ * @param {String} wikipedia 
+ * @param {String} youtube 
+ * @returns {Movie}
+ */
+
+function Movie({
+    name = null,
+    description = null,
+    likes = 0,
+    poster = null,
+    votes= 0,
+    wikipedia = null,
+    youtube = null
+}) {
+    this.name = name;
+    this.description = description;
+    this.likes = likes;
+    this.poster = poster;
+    this.votes= votes;
+    this.wikipedia = wikipedia;
+    this.youtube = youtube;
+}
+
+export default Movie;
