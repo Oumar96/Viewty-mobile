0ab115fe3789f75a6178c5b42d02f6fb8938bdde
Oumar <oumarba221296@hotmail.fr>
2021-04-14 22:58:28 -0400

    start expired room

---
+++ components/MovieDetails.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const MovieDetails = ({ props }) => {
+  const { movie = {} } = props;
+
+  return <></>;
+};
+
+export default MovieDetails;
---
+++ screens/ExpiredRoom.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View } from "react-native";
+//components
+import MovieDetails from "../components/MovieDetails.js";
+
+const ExpiredRoom = () => {
+  return (
+    <View>
+      <MovieDetails />
+    </View>
+  );
+};
+
+export default ExpiredRoom;
