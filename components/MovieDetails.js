0ad544225e3ac855a4218f9f1b18bd70fb81a466
Oumar <oumarba221296@hotmail.fr>
2021-04-15 22:27:16 -0400

    movie stuff

--- components/MovieDetails.js
+++ components/MovieDetails.js
@@ -1,9 +1,36 @@
 import React from "react";
+import { View, StyleSheet } from "react-native";
+import BaseImage from "../components/BaseImage.js";
 
 const MovieDetails = ({ props }) => {
-  const { movie = {} } = props;
+  const { room = {} } = props;
+  const movie = room.result.movie;
+  const users = room.participants.users;
+  return (
+    <View style={styles.movieDetails}>
+      <BaseImage style={styles.movieImage} source={movie.poster} />
+      <View style={styles.movieNameContainer}>
+        <Text style={styles.movieName}>{movie.name}</Text>
+      </View>
 
-  return <></>;
+      <View style={styles.participants}>
+        {users.map((user, index) => (
+          <Text key={index} style={styles.participantName}>
+            {/* {user} */} John Smith
+          </Text>
+        ))}
+      </View>
+    </View>
+  );
 };
 
 export default MovieDetails;
+
+const styles = StyleSheet.create({
+  movieDetails: {},
+  movieImage: {},
+  movieNameContainer: {},
+  movieName: {},
+  participants: {},
+  participantName: {},
+});
