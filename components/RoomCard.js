91cbf65b839b5882fa085c68ff4a0c199a2ee9ca
Oumar <oumarba221296@hotmail.fr>
2021-04-29 23:24:02 -0400

    refactor card

--- components/RoomCard.js
+++ components/RoomCard.js
@@ -16,7 +16,7 @@ import RoomsContext from "../contexts/RoomsContext.js";
 import BaseImage from "./BaseImage.js";
 
 const SCREEN_HEIGHT = Dimensions.get("window").height;
-const CARD_HEIGHT = SCREEN_HEIGHT / 3;
+const CARD_HEIGHT = SCREEN_HEIGHT / 3.5;
 const CARD_MARGIN = 32;
 
 /**
@@ -109,6 +109,13 @@ const RoomCard = (props) => {
   /***********
    * Methods
    ***********/
+  const getImageBorderStyle = (index) => {
+    return {
+      0: styles.roomCardImage__left,
+      1: styles.roomCardImage__center,
+      2: styles.roomCardImage__right,
+    }[index];
+  };
   const handleClickCard = () => {
     return {
       active: goToMovies,
@@ -141,23 +148,21 @@ const RoomCard = (props) => {
         style={styles.roomCardTouchable}
         onPress={handleClickCard}
       >
-        {/* <View style={styles.roomCardTitle}>
-          <View style={styles.participants}>
-            {users.map((user, index) => (
-              <Text key={index} style={styles.roomCardUser}>
-                John Smith
-              </Text>
-            ))}
-          </View>
-          <View style={styles.status}>
-            <Text style={styles[`statusText__${status}`]}>{status}</Text>
-          </View>
-        </View> */}
+        <Text style={[styles.status, styles[`statusText__${status}`]]}>
+          {status}
+        </Text>
+        <View style={styles.participants}>
+          {users.map((user, index) => (
+            <Text key={index} style={styles.roomCardUser}>
+              John Smith
+            </Text>
+          ))}
+        </View>
         <View style={styles.roomCardImages}>
           {isRoomExpired ? (
             <SharedElement id={`room-${room.id}`} style={styles.sharedElement}>
               <BaseImage
-                style={styles.roomCardImage}
+                style={styles.expiredRoomImage}
                 source={finalMovie.poster}
               />
             </SharedElement>
@@ -166,7 +171,7 @@ const RoomCard = (props) => {
               {threeMovies.map((movie, index) => (
                 <BaseImage
                   key={index}
-                  style={styles.roomCardImage}
+                  style={[styles.roomCardImage, getImageBorderStyle(index)]}
                   source={movie.poster}
                 />
               ))}
@@ -190,45 +195,63 @@ const styles = StyleSheet.create({
   },
   roomCardTouchable: {
     flex: 1,
-  },
-  roomCardTitle: {
-    flex: 2,
-    padding: 10,
-    flexDirection: "row",
+    position: "relative",
   },
   participants: {
-    flex: 4,
+    position: "absolute",
+    zIndex: 1000,
+    bottom: 0,
+    width: "100%",
+    backgroundColor: "rgba(0, 0, 0, 0.6)",
+    paddingLeft: 20,
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
   },
   status: {
-    flex: 1,
-  },
-  statusText__active: {
+    position: "absolute",
+    zIndex: 1000,
+    right: 20,
+    top: 10,
     fontSize: 12,
     fontWeight: "bold",
+  },
+  statusText__active: {
     color: "#22f253",
   },
   statusText__pending: {
-    fontSize: 12,
-    fontWeight: "bold",
     color: "#de8a02",
   },
   statusText__expired: {
-    fontSize: 12,
-    fontWeight: "bold",
     color: "red",
   },
   roomCardImages: {
-    flex: 6,
+    flex: 1,
     flexDirection: "row",
   },
   roomCardImage: {
     flex: 1,
     height: "100%",
   },
+  expiredRoomImage: {
+    flex: 1,
+    height: "100%",
+    borderRadius: 20,
+  },
+  roomCardImage__left: {
+    borderBottomLeftRadius: 20,
+    borderTopLeftRadius: 20,
+  },
+  roomCardImage__right: {
+    borderBottomRightRadius: 20,
+    borderTopRightRadius: 20,
+  },
+  roomCardImage__center: {
+    borderRadius: 0,
+  },
   roomCardUser: {
-    fontSize: 16,
+    fontSize: 26,
     fontWeight: "bold",
-    color: "black",
+    color: "white",
     marginBottom: 10,
   },
   sharedElement: {
