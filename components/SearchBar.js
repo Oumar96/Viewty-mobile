6bbc7a60b6e0b70593acbaa62fcef8d5ea4efd82
Oumar <oumarba221296@hotmail.fr>
2021-05-20 22:02:42 -0400

    moving search result from component

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -3,41 +3,34 @@ import { TextInput, StyleSheet, View, Text } from "react-native";
 import { AntDesign } from "@expo/vector-icons";
 
 const SearchBar = (props) => {
-  const { style = {} } = props;
+  const { style = {}, value = "", handleOnChange = () => {} } = props;
 
-  const [searchValue, setSearchValue] = useState("");
-  const [users, setUsers] = useState([]);
-  const usersArray = [
-    { id: 1, fullName: "John Travolta" },
-    { id: 2, fullName: "Jackie Chan" },
-    { id: 3, fullName: "Big Popa" },
-  ];
+  // const [searchValue, setSearchValue] = useState("");
+  // const [users, setUsers] = useState([]);
+  // const usersArray = [
+  //   { id: 1, fullName: "John Travolta" },
+  //   { id: 2, fullName: "Jackie Chan" },
+  //   { id: 3, fullName: "Big Popa" },
+  // ];
 
-  useEffect(() => {
-    const filteredNames = usersArray.filter(({ fullName }) =>
-      fullName.includes(searchValue)
-    );
-    setUsers(filteredNames);
-  }, [searchValue]);
+  // useEffect(() => {
+  //   const filteredNames = usersArray.filter(({ fullName }) =>
+  //     fullName.includes(searchValue)
+  //   );
+  //   setUsers(filteredNames);
+  // }, [searchValue]);
 
   return (
-    <>
-      <View style={[styles.searchBar, style]}>
-        <AntDesign name="search1" size={20} color="#cccccc" />
-        <TextInput
-          value={searchValue}
-          onChangeText={(text) => setSearchValue(text)}
-          placeholder="Search"
-          placeholderTextColor="#cccccc"
-          style={styles.searchBarInput}
-        />
-      </View>
-      <View>
-        {users.map((user) => (
-          <Text>{user.fullName}</Text>
-        ))}
-      </View>
-    </>
+    <View style={[styles.searchBar, style]}>
+      <AntDesign name="search1" size={20} color="#cccccc" />
+      <TextInput
+        value={value}
+        onChangeText={handleOnChange}
+        placeholder="Search"
+        placeholderTextColor="#cccccc"
+        style={styles.searchBarInput}
+      />
+    </View>
   );
 };
 
--- screens/CreateRoom.js
+++ screens/CreateRoom.js
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet } from "react-native";
 
 import SearchBar from "../components/SearchBar.js";
 
 const CreateRoom = () => {
+  const [searchValue, setSearchValue] = useState("");
+  const [users, setUsers] = useState([]);
+  const usersArray = [
+    { id: 1, fullName: "John Travolta" },
+    { id: 2, fullName: "Jackie Chan" },
+    { id: 3, fullName: "Big Popa" },
+  ];
+
+  useEffect(() => {
+    const filteredNames = usersArray.filter(({ fullName }) =>
+      fullName.includes(searchValue)
+    );
+    setUsers(filteredNames);
+  }, [searchValue]);
+
   return (
     <View style={styles.createRoom}>
-      <SearchBar style={styles.searchBar} />
+      <SearchBar
+        style={styles.searchBar}
+        value={searchValue}
+        handleOnChange={setSearchValue}
+      />
+      <View>
+        {users.map((user) => (
+          <Text>{user.fullName}</Text>
+        ))}
+      </View>
     </View>
   );
 };
